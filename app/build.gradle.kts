b8f45a723ebf77ff4ad62027a945eddef815b4be
mazharmnzoor4227-beep <mazharmnzoor4227@gmail.com>
2026-09-19 04:29:05 +0500

    Bump APK App Store to 1.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.mazhar.apkappstore"
         minSdk = 29
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.4.0"
+        versionCode = 6
+        versionName = "1.5.0"
 
         buildConfigField("String", "DEFAULT_BACKEND_URL", "\"${storeBackendUrl.replace("\"", "\\\"")}\"")
     }
